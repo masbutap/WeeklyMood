9f3c954f874f2ca626c2454bcf1c0989fe76f449
alizta27 <afdaall27@gmail.com>
2022-01-28 00:25:36 +0800

    perubahan radio tpi gak bisa

--- flow.js
+++ flow.js
@@ -11,41 +11,106 @@ const nextBtnFiv = document.querySelector('.next-4')
 const prevBtnSix = document.querySelector('.prev-5')
 const nextBtnSix = document.querySelector('.next-5')
 const prevBtnSev = document.querySelector('.prev-6')
-const submitBtnSev = document.querySelector('.submit')
+const submitBtn = document.querySelector('.submit')
+const progress = document.querySelectorAll('.step p')
+const progressCheck = document.querySelectorAll('.step .check')
+const bullet = document.querySelectorAll('.step .bullet')
+let max = 7
+let current = 1
 
 firtNextBtn.addEventListener("click", function () {
    slidePage.style.marginLeft = "-25%"
+   bullet[current - 1].classList.add("active")
+   progress[current - 1].classList.add("active")
+   progressCheck[current - 1].classList.add("active")
+   current += 1
 })
 nextBtnSec.addEventListener("click", function () {
    slidePage.style.marginLeft = "-50%"
+   bullet[current - 1].classList.add("active")
+   progress[current - 1].classList.add("active")
+   progressCheck[current - 1].classList.add("active")
+   current += 1
 })
 nextBtnTrd.addEventListener("click", function () {
    slidePage.style.marginLeft = "-75%"
+   bullet[current - 1].classList.add("active")
+   progress[current - 1].classList.add("active")
+   progressCheck[current - 1].classList.add("active")
+   current += 1
 })
 nextBtnFour.addEventListener("click", function () {
    slidePage.style.marginLeft = "-100%"
+   bullet[current - 1].classList.add("active")
+   progress[current - 1].classList.add("active")
+   progressCheck[current - 1].classList.add("active")
+   current += 1
 })
 nextBtnFiv.addEventListener("click", function () {
    slidePage.style.marginLeft = "-125%"
+   bullet[current - 1].classList.add("active")
+   progress[current - 1].classList.add("active")
+   progressCheck[current - 1].classList.add("active")
+   current += 1
 })
 nextBtnSix.addEventListener("click", function () {
    slidePage.style.marginLeft = "-150%"
+   bullet[current - 1].classList.add("active")
+   progress[current - 1].classList.add("active")
+   progressCheck[current - 1].classList.add("active")
+   current += 1
 })
+// submit buttonnya ---------------------
+submitBtn.addEventListener("click", function () {
+   bullet[current - 1].classList.add("active")
+   progress[current - 1].classList.add("active")
+   progressCheck[current - 1].classList.add("active")
+   current += 1
+   setTimeout(function () {
+      alert('okeokeoek')
+   }, 800)
+})
+//----------------------------------------
+// 
 prevBtnSev.addEventListener("click", function () {
    slidePage.style.marginLeft = "-125%"
+   bullet[current - 2].classList.remove("active")
+   progress[current - 2].classList.remove("active")
+   progressCheck[current - 2].classList.remove("active")
+   current -= 1
 })
 prevBtnSix.addEventListener("click", function () {
    slidePage.style.marginLeft = "-100%"
+   bullet[current - 2].classList.remove("active")
+   progress[current - 2].classList.remove("active")
+   progressCheck[current - 2].classList.remove("active")
+   current -= 1
 })
 prevBtnFiv.addEventListener("click", function () {
    slidePage.style.marginLeft = "-75%"
+   bullet[current - 2].classList.remove("active")
+   progress[current - 2].classList.remove("active")
+   progressCheck[current - 2].classList.remove("active")
+   current -= 1
 })
 prevBtnFour.addEventListener("click", function () {
    slidePage.style.marginLeft = "-50%"
+   bullet[current - 2].classList.remove("active")
+   progress[current - 2].classList.remove("active")
+   progressCheck[current - 2].classList.remove("active")
+   current -= 1
 })
 prevBtnTrd.addEventListener("click", function () {
    slidePage.style.marginLeft = "-25%"
+   bullet[current - 2].classList.remove("active")
+   progress[current - 2].classList.remove("active")
+   progressCheck[current - 2].classList.remove("active")
+   current -= 1
 })
 prevBtnSec.addEventListener("click", function () {
    slidePage.style.marginLeft = "0%"
+   bullet[current - 2].classList.remove("active")
+   progress[current - 2].classList.remove("active")
+   progressCheck[current - 2].classList.remove("active")
+   current -= 1
 })
